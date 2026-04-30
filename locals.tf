766296c5f88a2bbb13bfd2af728503c23ff10ca2
narendrakumar-sun <narendra.sunkara83@gmail.com>
2026-04-30 20:02:13 +0400

    terraform

--- locals.tf
+++ locals.tf
@@ -5,15 +5,14 @@ locals {
         Terraform = "true"
     }
         
-  ami_id = data.aws_ssm_parameter.ami_id.value
-  sg_id = data.aws_ssm_parameter.sg_id.value
-  private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
-  health_check_path = var.component == "frontend" ? "/" : "/health"
-  port_number = var.component == "frontend " ? 80 : 8080
+  ami_id = data.aws_ami.joindevops.id
   vpc_id = data.aws_ssm_parameter.vpc_id.value
-  frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
+  private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  sg_id = data.aws_ssm_parameter.sg_id.value
+  health_check_path = var.component == "frontend" ? "/" : "/health"
+  port_number = var.component == "frontend" ? 80 : 8080
   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
+  frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
   alb_listener_arn = var.component == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
-  host_header = var.component == "frontend" ? "${var.component}-${var.environment}.${var.domain_name}" : "${var.component}.backend_alb-${var.environment}.${var.domain_name}"
-
+  host_header = var.component == "frontend" ? "${var.component}-${var.environment}.${var.domain_name}" : "${var.component}.backend-alb-${var.environment}.${var.domain_name}"
 }
