03f03004fb1a4b0594ffd3dc2d093ba812ab3e6d
narendrakumar-sun <narendra.sunkara83@gmail.com>
2026-05-01 21:58:19 +0400

    terraform

--- main.tf
+++ main.tf
@@ -196,3 +196,15 @@ resource "aws_lb_listener_rule" "main" {
     }
   }
 }
+
+resource "terraform_data" "main_delete" {
+  triggers_replace = [
+    aws_instance.main.id
+  ]
+  depends_on = [aws_autoscaling_policy.main]
+  
+  # it executes in bastion
+  provisioner "local-exec" {
+    command = "aws ec2 terminate-instances --instance-ids ${aws_instance.main.id} "
+  }
+}
